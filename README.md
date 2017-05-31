705582652c8a0e4c1b1467a51a1ca86e41faa94f
paulomelila <paulovitor22@hotmail.com>
2017-05-31 09:39:38 -0700

    sample code

---
+++ README.md
@@ -0,0 +1,6 @@
+Document ▼
+https://goo.gl/gfOM4Z
+
+
+Mock Up UML ▼
+https://goo.gl/VcCITp
---
+++ SalaryConverter/app/src/main/java/com/example/paulo/salaryconverter/SalaryInfoActivity.java
@@ -0,0 +1,13 @@
+package com.example.paulo.salaryconverter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SalaryInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_salary_info);
+    }
+}
